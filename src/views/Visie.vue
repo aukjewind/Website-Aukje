897975a0c4d4b94f826b80e598dc4a3acef30fab
Aukje <aukje@wind.cd>
2023-01-12 17:29:04 +0100

    Update Visie.vue

--- src/views/Visie.vue
+++ src/views/Visie.vue
@@ -2,7 +2,7 @@
   <div class="visie">
     <h2 class="mt-3">Visie</h2>
     <p class="pt-3 mb-5">
-      Als eigenaresse van 3 honden snap ik maar al te goed dat u uw trouwe viervoeter niet zomaar aan de eerste de beste meegeeft.
+      Als eigenaresse van 4 honden snap ik maar al te goed dat u uw trouwe viervoeter niet zomaar aan de eerste de beste meegeeft.
       <br /><br />
       Wat ik u en uw hond(en) kan bieden is wat ik zelf van een ander verwacht als het om het uitlaten van mijn eigen honden zou gaan.
       <br /><br />
@@ -11,8 +11,7 @@
       &bull; Liefdevolle omgang met uw hond<br />
       &bull; Positieve leermethoden<br />
       &bull; Geduld<br />
-      &bull; Een fijne boswandeling in een rustige omgeving en stabiele groep<br />
-      &bull; Veel foto’s van uw hond(en)
+      &bull; Een fijne wandeling in een rustige omgeving en stabiele groep<br />
       <br /><br />
       Ik vind het ontzettend belangrijk dat iedere hond zich op zijn/haar gemak voelt binnen de groep en mij vertrouwd. Het vertrouwen van u in mij is belangrijk, maar dat van uw hond(en) misschien nog wel belangrijker.
     </p>
